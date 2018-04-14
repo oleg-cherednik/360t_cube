f01c3a406a38ee004d4bd061786f7e53199ecb7f
oleg.cherednik <oleg.cherednik@gmail.com>
2018-04-14 21:42:29 +0300

    test

--- src/main/java/cop/cube/CubeGame.java
+++ src/main/java/cop/cube/CubeGame.java
@@ -71,8 +71,8 @@ public final class CubeGame {
     }
 
     private void solve(Cube cube, Set<SquareShape> shapes, boolean useRelated) {
-        if(cubes.size() == 1)
-            return;
+//        if(cubes.size() == 1)
+//            return;
         if (shapes.isEmpty() && cube.isComplete()) {
             cubes.add(cube.clone());
             return;
